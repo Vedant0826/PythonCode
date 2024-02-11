4140ab528ec4e05bbc7188c88bda8cdf3e33a347
Vedant0826 <vedantd2685@gmail.com>
2024-02-11 11:15:58 +0530

    adding sleep method until certain task is complete

--- FileSorter.py
+++ FileSorter.py
@@ -1,12 +1,52 @@
-import os, sys
+import os
 import shutil
-
+from time import sleep
 
 
 if __name__ == "__main__":
     current_user = os.getlogin()
-    list_ = os.listdir(f"/home/{current_user}")
-    download_folder = os.path.isdir(f"/home/{current_user}/Downloads")
-    print(download_folder)
-    for i in list_:
-        print(i)
+    DOWNLOADS_FOLDER = f"/home/{current_user}/Downloads/"
+
+    download_folder_exists = os.path.isdir(DOWNLOADS_FOLDER)
+
+    #extension lists
+    file_ext = ('.c', '.cpp', '.py', '.html', '.css', '.js', '.java', '.txt', '.rs')
+    video_ext = ('.mp4', '.aac', '.mkv', '.mov')
+    music_ext = ('.mp3', '.wav', '.aac')
+    image_ext = ('.png', '.jpeg', '.jpg', '.gif', '.eps', '.dwg', '.psd')
+
+    #folders path for sorting 
+    PICTURES_FOLDER = f"/home/{current_user}/Pictures/"
+    VIDEOS_FOLDER = f"/home/{current_user}/Videos/"
+    MUSIC_FOLDER = f"/home/{current_user}/Music/"
+    DOCUMENTS_FOLDER = f"/home/{current_user}/Documents/"
+    PERSONAL_FOLDER = f"/home/{current_user}/Documents/Common/.MyPersonal/"
+
+
+    if download_folder_exists:
+        for item in os.listdir(DOWNLOADS_FOLDER):
+            
+            if item.endswith(file_ext):
+                    shutil.move(DOWNLOADS_FOLDER + item, DOCUMENTS_FOLDER)
+                    sleep()
+            
+            if item.endswith(video_ext):
+                    shutil.move(DOWNLOADS_FOLDER + item, VIDEOS_FOLDER)
+            
+            if item.endswith(music_ext):
+                    shutil.move(DOWNLOADS_FOLDER + item, MUSIC_FOLDER)
+
+            if item.endswith(image_ext):
+                    shutil.move(DOWNLOADS_FOLDER + item, PICTURES_FOLDER)
+
+            else:
+                if not os.path.exists(PERSONAL_FOLDER):
+                        print("no")
+                        os.makedirs(PERSONAL_FOLDER)
+                        shutil.move(DOWNLOADS_FOLDER + item, PERSONAL_FOLDER)      
+                else:
+                      shutil.move(DOWNLOADS_FOLDER + item, PERSONAL_FOLDER)      
+
+
+    else:
+        print("cant locate downloads folder")
